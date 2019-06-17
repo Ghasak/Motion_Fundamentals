c52fd2294334a318f1bd30e5af3ea2655a219223
Ghasak Ibrahim <mdotgh@gmail.com>
2019-06-18 06:33:36 +0900

    Now the E1 is a template - Working very well

--- src/E1_Diagonal_movement.py
+++ src/E1_Diagonal_movement.py
@@ -25,16 +25,19 @@ from sys import exit
 import pygame
 from pygame.locals import *
 from sys import exit
+import random
 
 # --------------------------------------------------------
 # define the RGB value for white,
 #  green, blue colour .
 white = (255, 255, 255)
+black = (0  , 0  ,0   )
 green = (0, 255, 0)
 blue  = (0, 0, 128)
 red   = (255, 0, 0)
 aquamarine = (0, 140, 255)
 colors = [(119,136,153),(220,20,60),(138,43,226),(205,92,92)]
+color_blinking = [(119,136,153),(119,136,153),(119,136,153),red, red,red]
 pygame.font.init() # you have to call this at the start,
                    # if you want to use this module.
 myfont = pygame.font.SysFont('SF Mono', 30)
@@ -45,15 +48,18 @@ class Particle:
         self.y = y
         self.size = size
         self.color = color
-        self.thickness = 0 # 1 to not fill, and 0 to fill
+        self.thickness = 3 # 1 to not fill, and 0 to fill
         self.VELOCITY_X = VELOCITY_X
         self.VELOCITY_Y = VELOCITY_Y
 
 
     def display(self):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.size, self.thickness)
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.size, 0)
+        pygame.draw.circle(screen, black, (int(self.x), int(self.y)), self.size, self.thickness)
         self.distance_traveled_label = myfont.render(f"Distance {int(self.x)} m.", True, green, blue)
         screen.blit(self.distance_traveled_label,(self.x,self.y))
+
+
 
     def move(self, time_passed):
         self.time_passed = time_passed
@@ -68,11 +74,25 @@ class Particle:
             self.VELOCITY_Y = -1 * self.VELOCITY_Y
         return self.x, self.y,self.VELOCITY_X,self.VELOCITY_Y
 
+class Environment():
+    def centerlines():
+        for i in range(100):
+            #pygame.draw.line(screen, black, (0, i*100), (i*100, HEIGHT), 5)
+            pygame.draw.line(screen,black, (0,i*100), (WIDTH,i*100),1)
+            pygame.draw.line(screen,black, (i*100,0), (i*100,HEIGHT),1)
+            x_label = myfont.render(f"{int(i*100)} m.", True, black)
+            y_label = myfont.render(f"{int(i*100)} m.", True, black)
+            screen.blit(x_label,(0,i*100))
+            screen.blit(y_label,((i*100,HEIGHT-30)))
+        pygame.draw.line(screen,red, (0,HEIGHT/2), (WIDTH,HEIGHT/2),5)
+        pygame.draw.line(screen,red, (WIDTH/2,0), (WIDTH/2, HEIGHT),5)
 
-WIDTH = 1800
+
+
+WIDTH = 2000
 HEIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(colors[0])
+#screen.fill(colors[0])
 #background = pygame.image.load(BACKGROUND_IMAGE_FILENAME).convert()
 
 
@@ -80,27 +100,32 @@ def Engine():
     # --------------------------------------------------------
     # Our clock object
     clock = pygame.time.Clock()
-    vxa,vya  = [130,200]
-    vxb,vyb  = [200,300]
-    vxc,vyc  = [323,100]
-    xa = ya = xb = yb = xc = yc = 0
+    vxa,vya  = [50,40]
+    vxb,vyb  = [30,25]
+    vxc,vyc  = [60,40]
+    xa = ya =  500 * random.random()
+    xb = yb =  1000 * random.random()
+    xc = yc =  200 * random.random()
     # Creating your particles with initial conditions
-    a = Particle(xa, ya, 20,colors[1],vxa,vya)
+
     b = Particle(xb,yb, 50, colors[2],vxb,vyb)
     c = Particle(xc,yc, 100, colors[3],vxc,vyc)
+
     # --------------------------------------------------------
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
         screen.fill(colors[0])
+        Environment.centerlines()
         # screen.set_colorkey((255,0,255))
         # screen.set_alpha(1)
         time_passed = clock.tick(60)  # This is a timer ,In a 1000 millesecond / 60 (max frame) = no. of
-
+        a = Particle(xa, ya, 20,random.choice(color_blinking) ,vxa,vya)
         a.display()
         b.display()
         c.display()
+
         xa,ya,vxa,vya  = a.move(time_passed)
         xb,yb,vxb,vyb  = b.move(time_passed)
         xc,yc,vxc,vyc  = c.move(time_passed)
